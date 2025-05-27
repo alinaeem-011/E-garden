7fb06db7b2dece113d0c3c906bf1562ee859c831
Ali Naeem <alinaeemkhan014@gmail.com>
2025-05-27 00:17:10 -0700

    e garden

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "340px",
+      md: "540px",
+      lg: "768px",
+      xl: "1180px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
