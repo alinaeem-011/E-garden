95c06c0ec8e3f80308434453cab444cdfba9a184
Ali Naeem <alinaeemkhan014@gmail.com>
2025-05-29 00:51:43 -0700

    7

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const navMenu = document.getElementById("nav-Menu");
+const navMenu = document.getElementById("nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
 const hamburger = document.getElementById("hamburger");
 
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("left-[0]");
+  hamburger.classList.toggle("ri-close-large-line");
 });
